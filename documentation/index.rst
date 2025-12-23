91a24699e94098f8c513c63284ed7d7ab76661c1
Pascal Pieters <pascalaldo@gmail.com>
2025-12-23 13:55:36 +0100

    Added documentation builder and action to create github pages

---
+++ documentation/index.rst
@@ -0,0 +1,22 @@
+biggr_maps
+=========================
+
+Python library to generate Escher maps, mainly used for the 
+`BiGGr Models Repository`_.
+
+.. _BiGGr Models Repository: https://biggr.org
+
+.. toctree::
+    :numbered: 3
+    :maxdepth: 3
+
+    basic_usage
+    API </autoapi/biggr_maps/index.rst>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ from setuptools import setup, find_packages
 
 # this is a trick to get the version before the package is installed
 directory = dirname(abspath(__file__))
-sys.path.insert(0, join(directory, "bigg_models"))
-version = "0.0.1"
+sys.path.insert(0, join(directory, "biggr_maps"))
+
+from biggr_maps import __version__
 
 setup(
     name="biggr_maps",
-    version=version,
+    version=__version__,
     author="Pascal Pieters",
     author_email="paspie@biosustain.dtu.dk",
     packages=find_packages(),
